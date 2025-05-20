fc2a9e41f13e6bc170fd6ef8fb7a42abffb93294
pipoon21 <pipoon22@hotmail.com>
2025-05-21 02:03:02 +0700

    Add unit tests for KeyColorGenerator.

---
+++ KeyColor.UnitTest/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿namespace KeyColor.UnitTest;
+
+public class KeyColorGeneratorTests {
+    [Fact]
+    public void TestGetUniqueColorFromString() {
+        KeyColorGenerator generator = new();
+        const string label = "testLabel";
+        GeneratedColor color1 = generator[label];
+        GeneratedColor color2 = generator[label];
+
+        Assert.Equal(color1.R, color2.R);
+        Assert.Equal(color1.G, color2.G);
+        Assert.Equal(color1.B, color2.B);
+    }
+
+    [Fact]
+    public void TestGetUniqueColorFromSpan() {
+        KeyColorGenerator generator = new();
+        byte[] key = [1, 2, 3];
+        GeneratedColor color1 = generator[key.AsSpan()];
+        GeneratedColor color2 = generator[key.AsSpan()];
+
+        Assert.Equal(color1.R, color2.R);
+        Assert.Equal(color1.G, color2.G);
+        Assert.Equal(color1.B, color2.B);
+    }
+
+    [Fact]
+    public void TestGetUniqueColorFromDifferentInputs() {
+        KeyColorGenerator generator = new();
+        const string label = "testLabel";
+        byte[] key = [1, 2, 3];
+
+        GeneratedColor colorFromLabel = generator[label];
+        GeneratedColor colorFromKey = generator[key.AsSpan()];
+
+        Assert.NotEqual(colorFromLabel.R, colorFromKey.R);
+        Assert.NotEqual(colorFromLabel.G, colorFromKey.G);
+        Assert.NotEqual(colorFromLabel.B, colorFromKey.B);
+    }
+
+    [Fact]
+    public void TestBrightnessRange() {
+        KeyColorGenerator generator = new();
+        const string label = "testLabel";
+        GeneratedColor color = generator[label];
+
+        int brightness = KeyColorGenerator.GetColorBrightness(color.R, color.G, color.B);
+
+        Assert.True(brightness >= generator.Brightness.Min && brightness <= generator.Brightness.Max);
+    }
+    
+    
+    
+}
