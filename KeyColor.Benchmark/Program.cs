b29ee672b77eb7600ab63c8404cb26bd1d0c6658
pipoon21 <pipoon22@hotmail.com>
2025-05-21 06:17:14 +0700

    Add benchmarking setup for multiple .NET versions and update project file to include references.

--- KeyColor.Benchmark/Program.cs
+++ KeyColor.Benchmark/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BenchmarkDotNet.Running;
+using System;
 
-Console.WriteLine("Hello, World!");
+namespace KeyColor.Benchmark;
+
+public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine($"Running benchmarks on {GetFrameworkVersion()}");
+        BenchmarkRunner.Run<ColorFromBenchmark>();
+    }
+
+    private static string GetFrameworkVersion()
+    {
+#if NET9_0
+        return ".NET 9.0";
+#elif NET8_0
+        return ".NET 8.0";
+#elif NETSTANDARD2_0
+        return ".NET Standard 2.0";
+#else
+        return "Unknown Framework";
+#endif
+    }
+}
